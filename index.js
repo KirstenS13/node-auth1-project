a67722cde9ccdbd0b96c80a8f50fe6b9bd05a439
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-02 20:37:59 -0400

    set up index.js server file

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "Something went wrong. Please try again later."
+    })
+})
+
+server.get("/", (req, res, next) => {
+    res.json({
+        message: "Hello, I am the server"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
